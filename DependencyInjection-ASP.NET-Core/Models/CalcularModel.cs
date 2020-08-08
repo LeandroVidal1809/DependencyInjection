a25b364020238f6d658088d47b659d3bfb174382
Leandro Vidal <leandrovidal92@gmail.com>
2020-08-08 15:27:20 -0300

    Agregamos nuevas clases Calcular .

---
+++ DependencyInjection-ASP.NET-Core/Models/CalcularModel.cs
@@ -0,0 +1,34 @@
+﻿using DependencyInjection_ASP.NET_Core.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+
+namespace DependencyInjection_ASP.NET_Core.Models
+{
+    public class CalcularModel
+    {
+        public string Calcular(CalculoViewModel viewmodel)
+        {
+            switch (viewmodel.TipoOperacion)
+            {
+                case Operacion.sumar:
+                    return (viewmodel.PrimerNumero + viewmodel.SegundoNumero).ToString();
+                case Operacion.restar:
+                    return (viewmodel.PrimerNumero - viewmodel.SegundoNumero).ToString();
+                case Operacion.multiplicar:
+                    return (viewmodel.PrimerNumero * viewmodel.SegundoNumero).ToString();
+                case Operacion.dividir:
+                    {
+                        if (viewmodel.SegundoNumero != 0)
+                        return (viewmodel.PrimerNumero + viewmodel.SegundoNumero).ToString();
+                        else
+                        return "No se puede dividir por cero";
+                    }
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+}
---
+++ DependencyInjection-ASP.NET-Core/ViewModel/CalculoViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection_ASP.NET_Core.ViewModel
+{
+    public class CalculoViewModel
+    {
+        public CalculoViewModel() { }
+
+        public decimal PrimerNumero { get; set; }
+        public decimal SegundoNumero { get; set; }
+        public Operacion TipoOperacion { get; set; }
+        public decimal Resultado { get; set; }
+    }
+
+    public enum Operacion
+    {
+        sumar = 1,
+        restar = 2,
+        multiplicar = 3,
+        dividir=4
+    }
+}
